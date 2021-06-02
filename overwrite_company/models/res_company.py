60936739564933683cdb5620193d6a5957dcccea
Cizaquita <cristian.izaquita@gmail.com>
2021-06-02 15:55:38 -0500

    Add: overwrite_company module. BoM copy. line_qty product_id.uom_id Fix

--- overwrite_company/models/res_company.py
+++ overwrite_company/models/res_company.py
@@ -69,7 +69,6 @@ class Company(models.Model):
                         'bom_id': bom_created.id,
                         'product_id': linea_bom.product_tmpl_id.product_variant_id.id,
                         'product_qty': line_qty,
-                        'product_uom_id': linea_bom.product_uom_id.id,
                         'product_qty_display': linea_bom.product_qty_display,
                         'product_uom_id_display': linea_bom.product_uom_id_display.id,
 
